9e9930a576ab1982bda418fd1017091b480f7552
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-01-31 19:28:22 -0600

    Add config template file

---
+++ myConfigTemplate.js
@@ -0,0 +1,10 @@
+var myConfig = {};
+
+myConfig.testAccountSid = "";
+myConfig.testAuthToken = "";
+myConfig.liveAccountSid = "";
+myConfig.liveAuthToken = "";
+myConfig.twilioNumber = ""; //+xxxxxxxxx
+myConfig.myNumber = "";
+
+module.exports = myConfig;
